cdf093a35562078b3f84ee31d141bb78133d998d
Topias Perälä <topias.perala1@gmail.com>
2026-02-19 15:11:36 +0200

    init

--- App.tsx
+++ App.tsx
@@ -46,7 +46,7 @@ export default function App() {
             </MotionProvider>
           </StepsProvider>
         </AuthProvider>
+      </ThemeProvider>
     </ErrorBoundary>
-    </ThemeProvider>
   );
 }
